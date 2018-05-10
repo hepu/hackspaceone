5c3bcb33a996dd218fc85cd7a3a453bfe1085133
Hans Gamarra <hans.gamarra1303@gmail.com>
2018-05-10 15:59:51 -0500

    added login button on index

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <a href='/login'>
+          Go to Login
+        </a>
       </div>
     );
   }
